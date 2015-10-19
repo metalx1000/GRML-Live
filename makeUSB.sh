c8b04186c6e356e4795128455ebc4f66878f4bb4
metalx1000 <github@filmsbykris.com>
2015-10-19 12:58:15 -0400

    multi format working

--- makeUSB.sh
+++ makeUSB.sh
@@ -10,17 +10,25 @@ release="2014.11"
 case "$1" in
         small32)
             url="http://download.grml.org/grml32-small_${release}.iso"
+            type="32small"
+            type2="32-small"
             ;;
 
         full32)
             url="http://download.grml.org/grml32-full_${release}.iso"
+            type="32full"
+            type2="32-full"
             ;;
 
         small64)
             url="http://download.grml.org/grml64-small_${release}.iso"
+            type="64small"
+            type2="64-small"
             ;;
         full64)
             url="http://download.grml.org/grml64-full_${release}.iso"
+            type="64full"
+            type2="64-full"
             ;;
         *)
             echo $"Usage: $0 {small32|full32|small64|full64}"
@@ -36,25 +44,36 @@ then
   update-initramfs -u
 fi
 
+echo "Possible Devices:"
+mount |grep vfat|awk '{print $1 " " $3}'
+
 echo "Where is the drive mounted?"
 echo "Example /mnt"
 read TARGET
 
-if [ TARGET = "" ]
+if [ $TARGET = "" ]
 then
     echo "A Mounted Location is Required"
     exit 1
 fi
 
 #variables
-ISO="/tmp/grml.iso"
+ISO="/tmp/grml${type2}_${release}.iso"
 mnt=/tmp/mnt
 
-echo "Which Device do you want to install to?"
-echo "Example: /dev/sdb"
-read dev
+dev="$(mount|grep "/media/metalx1000/B531-EB29"|awk '{print $1}')"
+dev=${dev::-1}
+echo "Is this the correct device? (y/n)"
+echo $dev
+read devq
 
-if [ dev = "" ]
+if [ $devq != "y" ]
+then
+  echo "Wrong device\nExiting"
+  exit 1
+fi
+
+if [ $dev = "" ]
 then
   echo "A Device is needed"
   exit 1
@@ -63,6 +82,7 @@ fi
 
 #get ISO
 wget -c "$url" -O "$ISO"
+rm -fr "$mnt"
 mkdir "$mnt"
 mount "$ISO" "$mnt"
 
@@ -70,10 +90,10 @@ mkdir -p ${TARGET}/boot/extlinux ${TARGET}/live
 extlinux -i ${TARGET}/boot/extlinux
 dd if=/usr/lib/syslinux/mbr/mbr.bin of=$dev #X is the drive letter
 
-cp -v $mnt/boot/grml32small/vmlinuz ${TARGET}/boot/vmlinuz
-cp -v $mnt/boot/grml32small/initrd.img ${TARGET}/boot/initrd
-cp -v $mnt/live/grml32-small/grml32-small.squashfs ${TARGET}/live
-umount $mnt
+cp -v $mnt/boot/grml${type}/vmlinuz ${TARGET}/boot/vmlinuz
+cp -v $mnt/boot/grml${type}/initrd.img ${TARGET}/boot/initrd
+cp -v $mnt/live/grml${type2}/*.squashfs ${TARGET}/live
+umount "$ISO"
 
 cp extlinux.conf ${TARGET}/boot/extlinux/extlinux.conf
 
